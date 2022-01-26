9c612c749e1e0bd7dcf0d90a6fe44e072e16d606
Iain Laird <ilaird@expediagroup.com>
2022-01-26 20:40:44 +0000

    Rename test asset

--- tests/model_tests.rs
+++ tests/model_tests.rs
@@ -7,7 +7,7 @@ const ROOT: &'static str = env!("CARGO_MANIFEST_DIR");
 
 #[test]
 fn deserialize_poke_api_species() {
-  let json = read(format!("{}/tests/assets/pikachu.json", ROOT)).expect("Read test data");
+  let json = read(format!("{}/tests/assets/raw_pikachu.json", ROOT)).expect("Read test data");
 
   let species = from_slice::<poke_models::PokemonSpecies>(&json).expect("Parse json");
 
