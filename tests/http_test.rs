2287391f7d343e99e1be803101aa617045fa00c6
Iain Laird <ikl@st-andrews.ac.uk>
2022-01-27 15:04:35 +0000

    Add advanced handler mocking test

--- tests/http_test.rs
+++ tests/http_test.rs
@@ -11,7 +11,7 @@ use truelayer_coding_challenge::{
 };
 
 mod mock_impl;
-use mock_impl::MockTranslationAPI;
+use mock_impl::{MockTranslationAPI, MockPokeAPI};
 
 const ROOT: &'static str = env!("CARGO_MANIFEST_DIR");
 
@@ -35,6 +35,33 @@ async fn test_basic_handler_networked() {
   let router = router(poke_client, MockTranslationAPI, cache);
 
   let res = request().path("/pokemon/pikachu").reply(&router).await;
+
+  assert!(res.status().is_success());
+
+  mock.assert();
+}
+
+#[tokio::test]
+async fn test_advanced_handler_networked() {
+  let mock_server = MockServer::start_async().await;
+
+  let mock = mock_server.mock(|when, then| {
+    when.method(GET)
+      .path("/translate/shakespeare")
+      .query_param("text", "When several of these POKéMON gather, their electricity could build and cause lightning storms.");
+    then.status(200)
+      .header("content-type", "application/json")
+      .body_from_file(format!("{}/tests/assets/raw_translation.json", ROOT));
+  });
+
+  let mut translation_client = API::new();
+  translation_client.override_uri(mock.server_address().to_string());
+  translation_client.disable_https();
+
+  let cache: Cache<(String, TranslationType), PokemonSpecies> = Cache::new(1_000);
+  let router = router(MockPokeAPI, translation_client, cache);
+
+  let res = request().path("/pokemon/translated/pikachu").reply(&router).await;
 
   assert!(res.status().is_success());
 
