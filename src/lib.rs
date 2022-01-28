5713b5cc89779a23ad767b223299a6376970541d
Iain Laird <ikl@st-andrews.ac.uk>
2022-01-28 02:37:23 +0000

    Add root level crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! Cached Pokemon description retrieval and subsequent fun translation of descriptions.
+//! 
+//! Utilises the Pokeapi and Funtranslations APIs.
+//! 
+//! Public API served by Warp, API connections made with Hyper. Caching provided
+//! by Moka. Utilises async/await where possible on a tokio runtime - runtime 
+//! provided by included libraries.
+
 pub mod util;
 pub mod models;
 pub mod api;
